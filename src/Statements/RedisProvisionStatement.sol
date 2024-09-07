4f1b4d856faecb765faa915f42389bfb47a8b43d
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-09-07 21:16:05 +0800

    redis provision statement

--- src/Statements/RedisProvisionStatement.sol
+++ src/Statements/RedisProvisionStatement.sol
@@ -2,7 +2,9 @@
 pragma solidity 0.8.26;
 
 import {Attestation} from "@eas/Common.sol";
-import {IEAS, AttestationRequest, AttestationRequestData} from "@eas/IEAS.sol";
+import {
+    IEAS, AttestationRequest, AttestationRequestData, RevocationRequest, RevocationRequestData
+} from "@eas/IEAS.sol";
 import {ISchemaRegistry} from "@eas/ISchemaRegistry.sol";
 import {IStatement} from "../IStatement.sol";
 
@@ -10,8 +12,7 @@ contract StringResultStatement is IStatement {
     struct StatementData {
         address user;
         uint256 size;
-        uint256 duration;
-        bytes32 serviceId;
+        uint64 duration;
         string url;
     }
 
@@ -21,12 +22,19 @@ contract StringResultStatement is IStatement {
         uint256 duration;
     }
 
+    struct ChangeData {
+        uint256 addedSize;
+        uint64 addedDuration;
+        string newUrl;
+    }
+
     error InvalidResultAttestation();
     error InvalidDemand();
+    error UnauthorizedCall();
 
-    string public constant SCHEMA_ABI = "address user, uint256 size, uint256 duration, bytes32 serviceId, string url";
+    string public constant SCHEMA_ABI = "address user, uint256 size, uint256 duration, string url";
     string public constant DEMAND_ABI = "address user, uint256 size, uint256 duration";
-    bool public constant IS_REVOCABLE = false;
+    bool public constant IS_REVOCABLE = true;
 
     constructor(IEAS _eas, ISchemaRegistry _schemaRegistry)
         IStatement(_eas, _schemaRegistry, SCHEMA_ABI, IS_REVOCABLE)
@@ -38,10 +46,42 @@ contract StringResultStatement is IStatement {
                 schema: ATTESTATION_SCHEMA,
                 data: AttestationRequestData({
                     recipient: msg.sender,
-                    expirationTime: 0,
-                    revocable: false,
+                    expirationTime: uint64(block.timestamp) + data.duration,
+                    revocable: true,
                     refUID: refUID,
                     data: abi.encode(data),
+                    value: 0
+                })
+            })
+        );
+    }
+
+    function reviseStatement(bytes32 statementUID, ChangeData calldata changeData) public returns (bytes32) {
+        Attestation memory statement = eas.getAttestation(statementUID);
+        StatementData memory statementData = abi.decode(statement.data, (StatementData));
+
+        if (statementData.user != msg.sender) revert UnauthorizedCall();
+
+        statementData.duration += changeData.addedDuration;
+        statementData.size += changeData.addedSize;
+
+        if (bytes(changeData.newUrl).length != 0) {
+            statementData.url = changeData.newUrl;
+        }
+
+        eas.revoke(
+            RevocationRequest({schema: ATTESTATION_SCHEMA, data: RevocationRequestData({uid: statementUID, value: 0})})
+        );
+
+        return eas.attest(
+            AttestationRequest({
+                schema: ATTESTATION_SCHEMA,
+                data: AttestationRequestData({
+                    recipient: msg.sender,
+                    expirationTime: statement.expirationTime - uint64(block.timestamp) + changeData.addedDuration,
+                    revocable: true,
+                    refUID: statement.refUID,
+                    data: abi.encode(statementData),
                     value: 0
                 })
             })
@@ -60,6 +100,9 @@ contract StringResultStatement is IStatement {
 
         DemandData memory demandData = abi.decode(demand, (DemandData));
         StatementData memory statementData = abi.decode(statement.data, (StatementData));
+
+        return demandData.user == statementData.user && demandData.size == statementData.size
+            && demandData.duration == statementData.duration;
     }
 
     function getSchemaAbi() public pure override returns (string memory) {
