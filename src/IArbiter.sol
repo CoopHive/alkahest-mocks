26182560b0edccc56e9f59e6594bba83c8d56be9
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-09-30 19:24:31 +0800

    format

--- src/IArbiter.sol
+++ src/IArbiter.sol
@@ -6,28 +6,36 @@ import {Attestation, DeadlineExpired, InvalidEAS} from "@eas/Common.sol";
 abstract contract IArbiter {
     bytes32 public immutable ATTESTATION_SCHEMA;
 
-    function _checkIntrinsic(Attestation memory statement) internal view returns (bool) {
+    function _checkIntrinsic(
+        Attestation memory statement
+    ) internal view returns (bool) {
         // check schema
         if (statement.schema != ATTESTATION_SCHEMA) return false;
         // check expired
-        if (statement.expirationTime != 0 && statement.expirationTime < block.timestamp) revert DeadlineExpired();
+        if (
+            statement.expirationTime != 0 &&
+            statement.expirationTime < block.timestamp
+        ) revert DeadlineExpired();
         // check revoked
         if (statement.revocationTime != 0) revert InvalidEAS();
 
         return true;
     }
 
-    function _checkIdentical(Attestation memory statement, bytes memory demand) public pure returns (bool) {
+    function _checkIdentical(
+        Attestation memory statement,
+        bytes memory demand
+    ) public pure returns (bool) {
         return keccak256(statement.data) == keccak256(demand);
     }
 
-    function checkStatement(Attestation memory statement, bytes memory demand, bytes32 counteroffer)
-        public
-        view
-        virtual
-        returns (bool)
-    {}
+    function checkStatement(
+        Attestation memory statement,
+        bytes memory demand,
+        bytes32 counteroffer
+    ) public view virtual returns (bool) {}
 
     function getSchemaAbi() public pure virtual returns (string memory) {}
+
     function getDemandAbi() public pure virtual returns (string memory) {}
 }
