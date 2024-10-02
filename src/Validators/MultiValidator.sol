2d0646ded12c5d36ca4b18b1585b9b58f52dfad3
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-02 16:14:28 +0800

    use new abstract contracts

--- src/Validators/MultiValidator.sol
+++ src/Validators/MultiValidator.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.26;
 import {Attestation} from "@eas/Common.sol";
 import {IEAS, AttestationRequest, AttestationRequestData} from "@eas/IEAS.sol";
 import {ISchemaRegistry} from "@eas/ISchemaRegistry.sol";
-import {IValidator} from "../IValidator.sol";
+import {IStatement} from "../IStatement.sol";
 import {IArbiter} from "../IArbiter.sol";
 import {ERC20PaymentStatement} from "../Statements/ERC20PaymentStatement.sol";
 
-contract ERC20PaymentFulfillmentValidator is IValidator {
+contract ERC20PaymentFulfillmentValidator is IStatement, IArbiter {
     struct ValidationData {
         bytes32 fulfilling;
         address[] arbiters;
@@ -20,29 +20,39 @@ contract ERC20PaymentFulfillmentValidator is IValidator {
         bytes[] demands;
     }
 
-    event ValidationCreated(bytes32 indexed validationUID, bytes32 indexed statementUID);
+    event ValidationCreated(
+        bytes32 indexed validationUID,
+        bytes32 indexed statementUID
+    );
 
     error InvalidStatement();
     error InvalidValidation();
 
-    string public constant SCHEMA_ABI = "bytes32 fulfilling, address[] arbiters, bytes[] demands";
-    string public constant DEMAND_ABI = "address[] arbiters, bytes[] demands";
-    bool public constant IS_REVOCABLE = true;
-
-    constructor(IEAS _eas, ISchemaRegistry _schemaRegistry, ERC20PaymentStatement _baseStatement)
-        IValidator(_eas, _schemaRegistry, SCHEMA_ABI, IS_REVOCABLE)
+    constructor(
+        IEAS _eas,
+        ISchemaRegistry _schemaRegistry,
+        ERC20PaymentStatement _baseStatement
+    )
+        IStatement(
+            _eas,
+            _schemaRegistry,
+            "bytes32 fulfilling, address[] arbiters, bytes[] demands",
+            true
+        )
     {}
 
-    function createValidation(bytes32 statementUID, ValidationData calldata validationData)
-        external
-        returns (bytes32 validationUID)
-    {
+    function createValidation(
+        bytes32 statementUID,
+        ValidationData calldata validationData
+    ) external returns (bytes32 validationUID) {
         Attestation memory statement = eas.getAttestation(statementUID);
 
         for (uint256 i = 0; i < validationData.arbiters.length; i++) {
             if (
                 !IArbiter(validationData.arbiters[i]).checkStatement(
-                    statement, validationData.demands[i], validationData.fulfilling
+                    statement,
+                    validationData.demands[i],
+                    validationData.fulfilling
                 )
             ) revert InvalidStatement();
         }
@@ -63,26 +73,23 @@ contract ERC20PaymentFulfillmentValidator is IValidator {
         emit ValidationCreated(validationUID, statementUID);
     }
 
-    function checkStatement(Attestation memory statement, bytes memory demand, bytes32 counteroffer)
-        public
-        view
-        override
-        returns (bool)
-    {
+    function checkStatement(
+        Attestation memory statement,
+        bytes memory demand,
+        bytes32 counteroffer
+    ) public view override returns (bool) {
         if (!_checkIntrinsic(statement)) return false;
-        ValidationData memory statement_ = abi.decode(statement.data, (ValidationData));
+        ValidationData memory statement_ = abi.decode(
+            statement.data,
+            (ValidationData)
+        );
         ValidationData memory demand_ = abi.decode(demand, (ValidationData));
 
-        return statement_.fulfilling == counteroffer
-            && keccak256(abi.encode(statement_.arbiters)) == keccak256(abi.encode(demand_.arbiters))
-            && keccak256(abi.encode(statement_.demands)) == keccak256(abi.encode(demand_.demands));
-    }
-
-    function getSchemaAbi() public pure override returns (string memory) {
-        return SCHEMA_ABI;
-    }
-
-    function getDemandAbi() public pure override returns (string memory) {
-        return DEMAND_ABI;
+        return
+            statement_.fulfilling == counteroffer &&
+            keccak256(abi.encode(statement_.arbiters)) ==
+            keccak256(abi.encode(demand_.arbiters)) &&
+            keccak256(abi.encode(statement_.demands)) ==
+            keccak256(abi.encode(demand_.demands));
     }
 }
