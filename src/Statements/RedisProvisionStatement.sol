98425f13f69a6c1b3e9a207696587a8c1f17f91f
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-02 16:17:35 +0800

    use new abstract contracts

--- src/Statements/RedisProvisionStatement.sol
+++ src/Statements/RedisProvisionStatement.sol
@@ -2,13 +2,12 @@
 pragma solidity 0.8.26;
 
 import {Attestation} from "@eas/Common.sol";
-import {
-    IEAS, AttestationRequest, AttestationRequestData, RevocationRequest, RevocationRequestData
-} from "@eas/IEAS.sol";
+import {IEAS, AttestationRequest, AttestationRequestData, RevocationRequest, RevocationRequestData} from "@eas/IEAS.sol";
 import {ISchemaRegistry} from "@eas/ISchemaRegistry.sol";
 import {IStatement} from "../IStatement.sol";
+import {IArbiter} from "../IArbiter.sol";
 
-contract StringResultStatement is IStatement {
+contract StringResultStatement is IStatement, IArbiter {
     struct StatementData {
         address user;
         uint256 size;
@@ -32,33 +31,47 @@ contract StringResultStatement is IStatement {
     error InvalidDemand();
     error UnauthorizedCall();
 
-    string public constant SCHEMA_ABI = "address user, uint256 size, uint256 duration, string url";
-    string public constant DEMAND_ABI = "address user, uint256 size, uint256 duration";
-    bool public constant IS_REVOCABLE = true;
-
-    constructor(IEAS _eas, ISchemaRegistry _schemaRegistry)
-        IStatement(_eas, _schemaRegistry, SCHEMA_ABI, IS_REVOCABLE)
+    constructor(
+        IEAS _eas,
+        ISchemaRegistry _schemaRegistry
+    )
+        IStatement(
+            _eas,
+            _schemaRegistry,
+            "address user, uint256 size, uint256 duration, string url",
+            true
+        )
     {}
 
-    function makeStatement(StatementData calldata data, bytes32 refUID) public returns (bytes32) {
-        return eas.attest(
-            AttestationRequest({
-                schema: ATTESTATION_SCHEMA,
-                data: AttestationRequestData({
-                    recipient: msg.sender,
-                    expirationTime: uint64(block.timestamp) + data.duration,
-                    revocable: true,
-                    refUID: refUID,
-                    data: abi.encode(data),
-                    value: 0
+    function makeStatement(
+        StatementData calldata data,
+        bytes32 refUID
+    ) public returns (bytes32) {
+        return
+            eas.attest(
+                AttestationRequest({
+                    schema: ATTESTATION_SCHEMA,
+                    data: AttestationRequestData({
+                        recipient: msg.sender,
+                        expirationTime: uint64(block.timestamp) + data.duration,
+                        revocable: true,
+                        refUID: refUID,
+                        data: abi.encode(data),
+                        value: 0
+                    })
                 })
-            })
-        );
+            );
     }
 
-    function reviseStatement(bytes32 statementUID, ChangeData calldata changeData) public returns (bytes32) {
+    function reviseStatement(
+        bytes32 statementUID,
+        ChangeData calldata changeData
+    ) public returns (bytes32) {
         Attestation memory statement = eas.getAttestation(statementUID);
-        StatementData memory statementData = abi.decode(statement.data, (StatementData));
+        StatementData memory statementData = abi.decode(
+            statement.data,
+            (StatementData)
+        );
 
         if (statementData.user != msg.sender) revert UnauthorizedCall();
 
@@ -70,44 +83,46 @@ contract StringResultStatement is IStatement {
         }
 
         eas.revoke(
-            RevocationRequest({schema: ATTESTATION_SCHEMA, data: RevocationRequestData({uid: statementUID, value: 0})})
-        );
-
-        return eas.attest(
-            AttestationRequest({
+            RevocationRequest({
                 schema: ATTESTATION_SCHEMA,
-                data: AttestationRequestData({
-                    recipient: msg.sender,
-                    expirationTime: statement.expirationTime - uint64(block.timestamp) + changeData.addedDuration,
-                    revocable: true,
-                    refUID: statement.refUID,
-                    data: abi.encode(statementData),
-                    value: 0
-                })
+                data: RevocationRequestData({uid: statementUID, value: 0})
             })
         );
+
+        return
+            eas.attest(
+                AttestationRequest({
+                    schema: ATTESTATION_SCHEMA,
+                    data: AttestationRequestData({
+                        recipient: msg.sender,
+                        expirationTime: statement.expirationTime -
+                            uint64(block.timestamp) +
+                            changeData.addedDuration,
+                        revocable: true,
+                        refUID: statement.refUID,
+                        data: abi.encode(statementData),
+                        value: 0
+                    })
+                })
+            );
     }
 
-    function checkStatement(Attestation memory statement, bytes memory demand, bytes32 /* counteroffer */ )
-        public
-        view
-        override
-        returns (bool)
-    {
+    function checkStatement(
+        Attestation memory statement,
+        bytes memory demand,
+        bytes32 /* counteroffer */
+    ) public view override returns (bool) {
         if (!_checkIntrinsic(statement)) return false;
 
         DemandData memory demandData = abi.decode(demand, (DemandData));
-        StatementData memory statementData = abi.decode(statement.data, (StatementData));
+        StatementData memory statementData = abi.decode(
+            statement.data,
+            (StatementData)
+        );
 
-        return demandData.user == statementData.user && demandData.size == statementData.size
-            && demandData.duration == statementData.duration;
-    }
-
-    function getSchemaAbi() public pure override returns (string memory) {
-        return SCHEMA_ABI;
-    }
-
-    function getDemandAbi() public pure override returns (string memory) {
-        return DEMAND_ABI;
+        return
+            demandData.user == statementData.user &&
+            demandData.size == statementData.size &&
+            demandData.duration == statementData.duration;
     }
 }
