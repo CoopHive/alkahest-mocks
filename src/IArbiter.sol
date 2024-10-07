cd35e1f930cb33816e148db31c5ea4419f6efe0c
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-07 16:39:51 +0800

    IArbiter as real interface, ArbiterUtils lib instead of base contract

--- src/IArbiter.sol
+++ src/IArbiter.sol
@@ -1,54 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
-
 import {Attestation} from "@eas/Common.sol";
 
-abstract contract IArbiter {
-    error DeadlineExpired();
-    error AttestationRevoked();
-    error InvalidSchema();
-
-    function _checkExpired(
-        Attestation memory statement
-    ) internal view returns (bool) {
-        return
-            statement.expirationTime != 0 &&
-            statement.expirationTime < block.timestamp;
-    }
-
-    function _checkRevoked(
-        Attestation memory statement
-    ) internal pure returns (bool) {
-        return statement.revocationTime != 0;
-    }
-
-    function _checkIntrinsic(
-        Attestation memory statement
-    ) internal view returns (bool) {
-        if (_checkExpired(statement)) revert DeadlineExpired();
-        if (_checkRevoked(statement)) revert AttestationRevoked();
-
-        return true;
-    }
-
-    function _checkIntrinsic(
-        Attestation memory statement,
-        bytes32 schema
-    ) internal view returns (bool) {
-        if (statement.schema != schema) revert InvalidSchema();
-        return _checkIntrinsic(statement);
-    }
-
-    function _checkIdentical(
-        Attestation memory statement,
-        bytes memory demand
-    ) public pure returns (bool) {
-        return keccak256(statement.data) == keccak256(demand);
-    }
-
+interface IArbiter {
     function checkStatement(
         Attestation memory statement,
         bytes memory demand,
         bytes32 counteroffer
-    ) public view virtual returns (bool) {}
+    ) external view returns (bool);
 }
