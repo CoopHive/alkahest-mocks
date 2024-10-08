e4effea80e4e6674c184ae8e782fbb536f6469f7
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-08 12:40:22 +0800

    no basestatement check if no basestatement

--- script/alkahest/src/codegen.ts
+++ script/alkahest/src/codegen.ts
@@ -131,9 +131,9 @@ const genArbiter = (
     out += "  constructor() {}\n\n";
   }
 
-  out += `  function checkStatement(Attestation memory statement, bytes memory demand, bytes32 counteroffer) public view override returns (bool) {
-    if (statement.schema != baseStatement.ATTESTATION_SCHEMA()) revert IncompatibleStatement();
-    DemandData memory demand_ = abi.decode(demand, (DemandData));
+  out += `  function checkStatement(Attestation memory statement, bytes memory demand, bytes32 counteroffer) public view override returns (bool) {\n`
+  if (opts.baseStatement) out += "    if (statement.schema != baseStatement.ATTESTATION_SCHEMA()) revert IncompatibleStatement();\n";
+  out += `    DemandData memory demand_ = abi.decode(demand, (DemandData));
     // implement custom checks here.
     // early revert with custom errors is recommended on failure.
     // remember that utility checks are available in IArbiter${opts.baseStatement ? ",\n    // and you can also use baseStatement.checkStatement() if appropriate." : ""}
