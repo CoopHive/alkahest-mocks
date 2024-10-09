431379e685b3a66b8594df23660b3267dbb0aca3
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-09 21:01:10 +0800

    rust port of gen_obligation

---
+++ script/alkahest_rs/target/debug/deps/alkahest_rs-b2591a1e00e19b61.d
@@ -0,0 +1,9 @@
+/Users/mlegls/dev/hive/alkahest-mocks/script/alkahest_rs/target/debug/deps/libalkahest_rs-b2591a1e00e19b61.rmeta: src/main.rs Cargo.toml
+
+/Users/mlegls/dev/hive/alkahest-mocks/script/alkahest_rs/target/debug/deps/alkahest_rs-b2591a1e00e19b61.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
