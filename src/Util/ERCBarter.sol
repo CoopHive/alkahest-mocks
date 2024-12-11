fa1459506c5c890eca91052c031a7b3a04b3b7d4
疒奀 <yz@mmon.co>
2024-12-11 12:03:39 +0800

    erc20 barter utils

---
+++ src/Util/ERCBarter.sol
@@ -0,0 +1,216 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import {Attestation} from "@eas/Common.sol";
+import {IEAS} from "@eas/IEAS.sol";
+import {ERC20PaymentFulfillmentArbiter} from "../Validators/ERC20PaymentFulfillmentArbiter.sol";
+import {ERC20PaymentObligation} from "../Statements/ERC20PaymentObligation.sol";
+import {IERC20Permit} from "@openzeppelin/token/ERC20/extensions/IERC20Permit.sol";
+
+contract ERC20Barter {
+    IEAS internal eas;
+    ERC20PaymentObligation internal erc20Payment;
+    ERC20PaymentFulfillmentArbiter internal erc20Fulfillment;
+
+    error CouldntCollectPayment();
+
+    constructor(
+        address _eas,
+        address payable _erc20Payment,
+        address _erc20Fulfillment
+    ) {
+        eas = IEAS(_eas);
+        erc20Payment = ERC20PaymentObligation(_erc20Payment);
+        erc20Fulfillment = ERC20PaymentFulfillmentArbiter(_erc20Fulfillment);
+    }
+
+    function permitAndBuyWithErc20(
+        address token,
+        uint256 amount,
+        address arbiter,
+        bytes memory demand,
+        uint64 expiration,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (bytes32) {
+        IERC20Permit tokenC = IERC20Permit(token);
+        tokenC.permit(
+            msg.sender,
+            address(erc20Payment),
+            amount,
+            block.timestamp + 1,
+            v,
+            r,
+            s
+        );
+        return
+            erc20Payment.makeStatement(
+                ERC20PaymentObligation.StatementData({
+                    token: token,
+                    amount: amount,
+                    arbiter: arbiter,
+                    demand: demand
+                }),
+                expiration,
+                bytes32(0)
+            );
+    }
+
+    function permitAndPayWithErc20(
+        address token,
+        uint256 amount,
+        bytes32 item,
+        uint64 expiration,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (bytes32) {
+        IERC20Permit tokenC = IERC20Permit(token);
+        tokenC.permit(
+            msg.sender,
+            address(erc20Payment),
+            amount,
+            block.timestamp + 1,
+            v,
+            r,
+            s
+        );
+        return
+            erc20Payment.makeStatement(
+                ERC20PaymentObligation.StatementData({
+                    token: token,
+                    amount: amount,
+                    arbiter: address(0),
+                    demand: ""
+                }),
+                expiration,
+                item
+            );
+    }
+
+    function _buyErc20ForErc20(
+        address bidToken,
+        uint256 bidAmount,
+        address askToken,
+        uint256 askAmount,
+        uint64 expiration
+    ) internal returns (bytes32) {
+        return
+            erc20Payment.makeStatement(
+                ERC20PaymentObligation.StatementData({
+                    token: bidToken,
+                    amount: bidAmount,
+                    arbiter: address(erc20Fulfillment),
+                    demand: abi.encode(
+                        ERC20PaymentFulfillmentArbiter.DemandData({
+                            token: askToken,
+                            amount: askAmount
+                        })
+                    )
+                }),
+                expiration,
+                bytes32(0)
+            );
+    }
+
+    function _payErc20ForErc20(
+        bytes32 buyAttestation
+    ) internal returns (bytes32) {
+        Attestation memory bid = eas.getAttestation(buyAttestation);
+        ERC20PaymentFulfillmentArbiter.DemandData memory demand = abi.decode(
+            bid.data,
+            (ERC20PaymentFulfillmentArbiter.DemandData)
+        );
+        bytes32 sellAttestation = erc20Payment.makeStatement(
+            ERC20PaymentObligation.StatementData({
+                token: demand.token,
+                amount: demand.amount,
+                arbiter: address(0),
+                demand: ""
+            }),
+            0,
+            buyAttestation
+        );
+
+        if (!erc20Payment.collectPayment(buyAttestation, sellAttestation)) {
+            revert CouldntCollectPayment();
+        }
+
+        return sellAttestation;
+    }
+
+    function permitAndBuyErc20ForErc20(
+        address bidToken,
+        uint256 bidAmount,
+        address askToken,
+        uint256 askAmount,
+        uint64 expiration,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (bytes32) {
+        IERC20Permit bidTokenC = IERC20Permit(bidToken);
+        bidTokenC.permit(
+            msg.sender,
+            address(erc20Payment),
+            bidAmount,
+            block.timestamp + 1,
+            v,
+            r,
+            s
+        );
+        return
+            _buyErc20ForErc20(
+                bidToken,
+                bidAmount,
+                askToken,
+                askAmount,
+                expiration
+            );
+    }
+
+    function permitAndPayErc20ForErc20(
+        bytes32 buyAttestation,
+        address askToken,
+        uint256 askAmount,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (bytes32) {
+        IERC20Permit askTokenC = IERC20Permit(askToken);
+        askTokenC.permit(
+            msg.sender,
+            address(erc20Payment),
+            askAmount,
+            block.timestamp + 1,
+            v,
+            r,
+            s
+        );
+        return _payErc20ForErc20(buyAttestation);
+    }
+
+    function buyErc20ForErc20(
+        address bidToken,
+        uint256 bidAmount,
+        address askToken,
+        uint256 askAmount,
+        uint64 expiration
+    ) external returns (bytes32) {
+        return
+            _buyErc20ForErc20(
+                bidToken,
+                bidAmount,
+                askToken,
+                askAmount,
+                expiration
+            );
+    }
+
+    function payErc20ForErc20(
+        bytes32 buyAttestation
+    ) external returns (bytes32) {
+        return _payErc20ForErc20(buyAttestation);
+    }
+}
