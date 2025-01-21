57448682f423fa0e9fe9b2836cf06e758709b2e8
疒奀 <yz@mmon.co>
2025-01-21 23:55:13 +0800

    pay erc1155 for each

--- src/Utils/ERC1155BarterCrossToken.sol
+++ src/Utils/ERC1155BarterCrossToken.sol
@@ -4,27 +4,39 @@ pragma solidity 0.8.26;
 import {ERC1155BarterUtils} from "./ERC1155BarterUtils.sol";
 import {IEAS} from "@eas/IEAS.sol";
 import {Attestation} from "@eas/Common.sol";
+import {ERC20EscrowObligation} from "../Statements/ERC20EscrowObligation.sol";
+import {ERC20PaymentObligation} from "../Statements/ERC20PaymentObligation.sol";
+import {ERC721EscrowObligation} from "../Statements/ERC721EscrowObligation.sol";
+import {ERC721PaymentObligation} from "../Statements/ERC721PaymentObligation.sol";
 import {ERC1155EscrowObligation} from "../Statements/ERC1155EscrowObligation.sol";
 import {ERC1155PaymentObligation} from "../Statements/ERC1155PaymentObligation.sol";
-import {ERC20PaymentObligation} from "../Statements/ERC20PaymentObligation.sol";
-import {ERC721PaymentObligation} from "../Statements/ERC721PaymentObligation.sol";
+import {TokenBundleEscrowObligation} from "../Statements/TokenBundleEscrowObligation.sol";
 import {TokenBundlePaymentObligation} from "../Statements/TokenBundlePaymentObligation.sol";
 
 contract ERC1155BarterCrossToken is ERC1155BarterUtils {
     ERC20PaymentObligation internal erc20Payment;
+    ERC20EscrowObligation internal erc20Escrow;
+    ERC721EscrowObligation internal erc721Escrow;
     ERC721PaymentObligation internal erc721Payment;
+    TokenBundleEscrowObligation internal bundleEscrow;
     TokenBundlePaymentObligation internal bundlePayment;
 
     constructor(
         IEAS _eas,
         ERC1155EscrowObligation _erc1155Escrow,
         ERC1155PaymentObligation _erc1155Payment,
+        ERC20EscrowObligation _erc20Escrow,
         ERC20PaymentObligation _erc20Payment,
+        ERC721EscrowObligation _erc721Escrow,
         ERC721PaymentObligation _erc721Payment,
+        TokenBundleEscrowObligation _bundleEscrow,
         TokenBundlePaymentObligation _bundlePayment
     ) ERC1155BarterUtils(_eas, _erc1155Escrow, _erc1155Payment) {
+        erc20Escrow = _erc20Escrow;
         erc20Payment = _erc20Payment;
+        erc721Escrow = _erc721Escrow;
         erc721Payment = _erc721Payment;
+        bundleEscrow = _bundleEscrow;
         bundlePayment = _bundlePayment;
     }
 
@@ -106,5 +118,81 @@ contract ERC1155BarterCrossToken is ERC1155BarterUtils {
                 msg.sender,
                 msg.sender
             );
+    }
+
+    function payErc1155ForErc20(
+        bytes32 buyAttestation
+    ) external returns (bytes32) {
+        Attestation memory bid = eas.getAttestation(buyAttestation);
+        ERC20EscrowObligation.StatementData memory escrowData = abi.decode(
+            bid.data,
+            (ERC20EscrowObligation.StatementData)
+        );
+        ERC1155PaymentObligation.StatementData memory demand = abi.decode(
+            escrowData.demand,
+            (ERC1155PaymentObligation.StatementData)
+        );
+
+        bytes32 sellAttestation = erc1155Payment.makeStatementFor(
+            demand,
+            msg.sender,
+            msg.sender
+        );
+
+        if (!erc20Escrow.collectPayment(buyAttestation, sellAttestation)) {
+            revert CouldntCollectPayment();
+        }
+
+        return sellAttestation;
+    }
+
+    function payErc1155ForErc721(
+        bytes32 buyAttestation
+    ) external returns (bytes32) {
+        Attestation memory bid = eas.getAttestation(buyAttestation);
+        ERC20EscrowObligation.StatementData memory escrowData = abi.decode(
+            bid.data,
+            (ERC20EscrowObligation.StatementData)
+        );
+        ERC1155PaymentObligation.StatementData memory demand = abi.decode(
+            escrowData.demand,
+            (ERC1155PaymentObligation.StatementData)
+        );
+
+        bytes32 sellAttestation = erc1155Payment.makeStatementFor(
+            demand,
+            msg.sender,
+            msg.sender
+        );
+
+        if (!erc721Escrow.collectPayment(buyAttestation, sellAttestation)) {
+            revert CouldntCollectPayment();
+        }
+
+        return sellAttestation;
+    }
+
+    function payErc1155ForBundle(
+        bytes32 buyAttestation
+    ) external returns (bytes32) {
+        Attestation memory bid = eas.getAttestation(buyAttestation);
+        TokenBundleEscrowObligation.StatementData memory escrowData = abi
+            .decode(bid.data, (TokenBundleEscrowObligation.StatementData));
+        TokenBundlePaymentObligation.StatementData memory demand = abi.decode(
+            escrowData.demand,
+            (TokenBundlePaymentObligation.StatementData)
+        );
+
+        bytes32 sellAttestation = bundlePayment.makeStatementFor(
+            demand,
+            msg.sender,
+            msg.sender
+        );
+
+        if (!bundleEscrow.collectPayment(buyAttestation, sellAttestation)) {
+            revert CouldntCollectPayment();
+        }
+
+        return sellAttestation;
     }
 }
