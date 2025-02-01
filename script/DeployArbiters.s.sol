e81f8b8d30cb8d6cc3adb1bcb1671524a6cf4880
疒奀 <yz@mmon.co>
2025-02-01 16:03:19 +0800

    deploy arbiters

---
+++ script/DeployArbiters.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import "forge-std/Script.sol";
+
+import {TrustedPartyArbiter} from "../src/Validators/TrustedPartyArbiter.sol";
+import {TrivialArbiter} from "../src/Validators/TrivialArbiter.sol";
+
+contract DeployArbiters is Script {
+    function run() external {
+        // Load environment variables
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy
+        TrustedPartyArbiter trustedPartyArbiter = new TrustedPartyArbiter();
+        TrivialArbiter trivialArbiter = new TrivialArbiter();
+
+        vm.stopBroadcast();
+
+        // Print deployed addresses
+        console.log("\nValidator Contracts:");
+        console.log("TrustedPartyArbiter:", address(trustedPartyArbiter));
+        console.log("TrivialArbiter:", address(trivialArbiter));
+    }
+}
