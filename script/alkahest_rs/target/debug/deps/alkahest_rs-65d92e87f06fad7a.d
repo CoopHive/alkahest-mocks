cf16115e87b861cf744ae74cbcac5a18821ec05b
疒奀 <yz@mmon.co>
2025-02-24 23:28:28 +0800

    build

---
+++ script/alkahest_rs/target/debug/deps/alkahest_rs-65d92e87f06fad7a.d
@@ -0,0 +1,10 @@
+/Users/mlegls/dev/hive/alkahest-mocks/script/alkahest_rs/target/debug/deps/libalkahest_rs-65d92e87f06fad7a.rmeta: src/main.rs src/codegen.rs Cargo.toml
+
+/Users/mlegls/dev/hive/alkahest-mocks/script/alkahest_rs/target/debug/deps/alkahest_rs-65d92e87f06fad7a.d: src/main.rs src/codegen.rs Cargo.toml
+
+src/main.rs:
+src/codegen.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ script/alkahest_rs/target/debug/deps/alkahest_rs-f0e02c55a96cadcc.d
@@ -0,0 +1,10 @@
+/Users/mlegls/dev/hive/alkahest-mocks/script/alkahest_rs/target/debug/deps/libalkahest_rs-f0e02c55a96cadcc.rmeta: src/main.rs src/codegen.rs Cargo.toml
+
+/Users/mlegls/dev/hive/alkahest-mocks/script/alkahest_rs/target/debug/deps/alkahest_rs-f0e02c55a96cadcc.d: src/main.rs src/codegen.rs Cargo.toml
+
+src/main.rs:
+src/codegen.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
