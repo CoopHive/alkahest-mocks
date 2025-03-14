ff21a5ed3fe8b840d8e9b9c910138a741e6b40d1
疒奀 <yz@mmon.co>
2025-03-14 14:00:00 +0800

    make dir interchangable

--- script/replace_jsons.py
+++ script/replace_jsons.py
@@ -2,15 +2,16 @@
 import os
 import shutil
 
-current_contracts = os.listdir("alkahest-rs/src/contracts")
+dir = "alkahest-ts"
+current_contracts = os.listdir(f"{dir}/src/contracts")
 
 #%%
 contracts = os.listdir("out")
 contracts = [c for c in contracts if c.replace("sol", "json") in current_contracts]
 
 #%%
-os.makedirs("alkahest-rs/src/contracts/bak", exist_ok=True)
+os.makedirs(f"{dir}/src/contracts/bak", exist_ok=True)
 for contract in contracts:
     json = contract.replace("sol", "json")
-    os.rename(f"alkahest-rs/src/contracts/{json}", f"alkahest-rs/src/contracts/bak/{json}")
-    shutil.copy(f"out/{contract}/{json}", f"alkahest-rs/src/contracts/{json}")
+    os.rename(f"{dir}/src/contracts/{json}", f"{dir}/src/contracts/bak/{json}")
+    shutil.copy(f"out/{contract}/{json}", f"{dir}/src/contracts/{json}")
